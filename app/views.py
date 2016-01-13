0ab4d605a686612ec814ff040d716eb81e5d2aca
Paween Theanvanichpant <paween.the@gmail.com>
2016-01-14 00:44:52 +0700

    Add navbar and signup page roughly

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
+from flask import render_template, flash, redirect, session, url_for, request
+from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  return render_template('index.html.j2')
 
-
-  
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  if request.method == 'GET':
+    return render_template('signup.html.j2')
