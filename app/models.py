09e0926c18f3785dfac26ef5bb9395ba57d7ee7e
Paween Theanvanichpant <paween.the@gmail.com>
2016-01-14 17:33:02 +0700

    Add password to User model

--- app/models.py
+++ app/models.py
@@ -2,11 +2,12 @@ from app import db
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
-  first_name = db.Column(db.String(64))
-  last_name = db.Column(db.String(64))
-  email = db.Column(db.String(120), index=True, unique=True)
-  student_id = db.Column(db.String(7), unique=True)
-  phone_num = db.Column(db.String(10), unique=True)
+  first_name = db.Column(db.String(64), nullable=False)
+  last_name = db.Column(db.String(64), nullable=False)
+  email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+  student_id = db.Column(db.String(7), unique=True, nullable=False)
+  phone_num = db.Column(db.String(10), unique=True, nullable=False)
+  password = db.Column(db.String, nullable=False)
 
   def __repr__(self):
     return '<User %r>' % (self.email)
