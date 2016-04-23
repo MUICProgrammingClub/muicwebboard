477d7e512643d3e3aa3a75189e946d81bae818b2
Paween Theanvanichpant <paween.the@gmail.com>
2016-04-23 16:37:15 +0700

    Define ability for users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,8 +7,52 @@ class Ability
     user_role = UserRole.where(user_id: user.id).order(:role_id).first
 
     if user_role.nil? # if guest user
-
+      can :read, [Course, Instructor, User, Major]
+      can :index, [Lecture, Review]
     else
+      if user_role.admin?
+        can :manage, :all
+      elsif user_role.moderator?
+        can :read, [Course, Instructor, User, Lecture, Review, Major]
+        can :create, [User, Lecture, Review]
+
+        can :update, Lecture do |lecture|
+          lecture.user == user
+        end
+
+        can :delete, Lecture do |lecture|
+          lecture.user == user
+        end
+
+        can :update, Review do |review|
+          review.user == user
+        end
+
+        can :delete, Review do |review|
+          review.user == user
+        end
+
+      elsif user_role.member?
+        can :read, [Course, Instructor, User, Lecture, Review, Major]
+        can :create, [Lecture, Review]
+
+        can :update, Lecture do |lecture|
+          lecture.user == user
+        end
+
+        can :delete, Lecture do |lecture|
+          lecture.user == user
+        end
+
+        can :update, Review do |review|
+          review.user == user
+        end
+
+        can :delete, Review do |review|
+          review.user == user
+        end
+
+      end
 
     end
   end
